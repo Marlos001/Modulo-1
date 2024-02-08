67d662fb8d0aebe1f240e56c52c88e9ee9176bac
Marlos Guedes <marlos.guedes@sou.inteli.edu.br>
2024-02-08 20:13:25 -0300

    Publicação da entrega ponderada Cristiano Semana 01

---
+++ Teste.js
@@ -0,0 +1,12 @@
+var nota1 = 4 // Definição da Variavel Nota 1
+var nota2 = 6 // Definição da Variavel Nota 2
+var soma = nota1+nota2 // Definição da Variavel da Soma das Notas
+var media = soma/2 // Definição da Variavel da média das Notas
+console.log(media); // "Print" da Média calculada
+if (media >= 7){ // se a Média calculada for maior que 7 o console "imprime" a string "Você passou!"
+    console.log('Você passou!');
+
+}
+else { // se a Média calculada NÃO for maior que 7 o console "imprime" a string "Reprovado!"
+    console.log('Reprovado!');
+}
